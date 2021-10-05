d93b0ef196658814d98165bafa9fef7675911b62
adval25 <adval25@gmail.com>
2021-10-05 15:11:44 -0600

    lab update 1.6

--- src/main.c
+++ src/main.c
@@ -4,15 +4,15 @@
 #include "history.h"
 #define MAX 300 // max amount of chars for user input string
 
-int main (void) {
-
-  // update
-  printf("Update 1:\n");
-  
+void main (void) {
   // Variables for user/char input
   int c, n;
   char input_str[MAX]; // user input string
   int b_slash = 0; // checks if the character entered is a '\'
+
+  char cp = '2';
+  int conv = (int)(cp - 48);
+  printf("Converted Char: %d\n", conv);
   
   // Receive user input
   printf("Type in a word & press ENTER: \n");
@@ -40,11 +40,38 @@ int main (void) {
     }
     c = getchar();
   }
-  
-  char *s_ptr = input_str;
+  char *s_ptr = input_str; // Pointer to user input string
   char **n_tokens = tokenize(s_ptr);
-  printf("Num Words: %d\n", count_words(s_ptr));
-  printf("Now Printing Tokens:\n");
-  print_tokens(n_tokens);
+  
+  // Prompt user for further action
+  printf("Type in a character to run a function: p- print_tokens, g- get_token, w- word_count\n");
+  c = getchar();
+
+  // Print Tokens
+  if (c == 112) {
+    printf("Now Printing Tokens:\n");
+    print_tokens(n_tokens);
+  } else if (c == 103) { // get_token
+    int id = 0, num_words = count_words(s_ptr) - 1;
+    printf("Press '[' or ']' keys to select index and press ENTER. Press EOF to get token:\n");
+    printf("'[' - Decrease id, ']' - Increase id\n");
+    printf("Current id: %d\n", id);
+    c = getchar();
+    while (c != EOF) {
+      if (c == 91 || c == 93) {
+	if (c == 91) { id--; } else { id++; }
+	if (id < 0) { id = 0; }
+	if (id > num_words) { id = num_words; }
+	printf("Current id: %d\n", id);
+      }
+      c = getchar();
+    }
+    printf("Token at id=%d: %s\n", id, get_token(n_tokens, id));
+  } else if (c == 119) { // print word_count
+    printf("Number of words in string: %d\n", count_words(s_ptr));
+  } else {
+    printf("Invalid command. Now exiting program\n");
+  }
   free_tokens(n_tokens);
+  printf("Thank you. Goodbye!\n");
 }
